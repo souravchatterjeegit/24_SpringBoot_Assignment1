e2a782b1b19d828929c64e08d94a0525f15b4662
souravchatterjeegit <sourav.chatterjee5@cognizant.com>
2018-10-29 11:50:26 +0530

    Added

--- src/test/java/com/cts/TestBooks.java
+++ src/test/java/com/cts/TestBooks.java
@@ -1,11 +1,31 @@
 package com.cts;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class TestBooks {
 	@Autowired
 	BookRepository myBookRepository;
+	
+	@Test
+	public void testGetBooks() throws Exception {
+		List<Book> bookList = myBookRepository.loadBooks();
+		Assert.assertNotEquals(null, bookList);
+	}
+	
+	@Test
+	public void testSearchBook() throws Exception {
+		List<Book> searchedBook = myBookRepository.searchBook("chem");
+		Assert.assertNotEquals(null, searchedBook);
+		Assert.assertEquals(2, searchedBook.size());
+	}
 }
--- src/test/java/com/cts/TestSubjects.java
+++ src/test/java/com/cts/TestSubjects.java
@@ -1,19 +1,35 @@
 package com.cts;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ComponentScan("com.cts")
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class TestSubjects {
 	@Autowired
 	SubjectRepository mySubjectRepository;
 	
 	@Test
-	public void testSearchSubject(){
-		mySubjectRepository.searchSubject("chem");
+	public void testGetSubjects() throws Exception {
+		List<Subject> subjectList = mySubjectRepository.loadSubjects();
+		Assert.assertNotEquals(null, subjectList);
+	}
+	
+	@Test
+	public void testSearchSubject() throws Exception {
+		List<Subject> searchedSubject = mySubjectRepository.searchSubject("c");
+		Assert.assertNotEquals(null, searchedSubject);
+		Assert.assertEquals(2, searchedSubject.size());
 	}
 }
